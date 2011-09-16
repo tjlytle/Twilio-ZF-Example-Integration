55b73128f0a570738d11ce7ea6d1b074eb17496d
Tim Lytle <tim@timlytle.net>
2011-09-16 00:52:12 -0400

    Updated

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,29 @@
 <?php
-
 class IndexController extends Zend_Controller_Action
 {
-
+    protected $twilio;
+    
     public function init()
     {
-        /* Initialize action controller here */
+        $this->twilio = $this->getFrontController()->getParam('bootstrap')->getResource('twilio');
+        
+        $contextSwitch = $this->getHelper('contextSwitch');
+        $contextSwitch->addContext('twiml', array('suffix' => 'twiml'));
+        $contextSwitch->addHeader('twiml', 'Content-Type', 'application/xml');
+        $contextSwitch->addActionContext('index', 'twiml');
+        $contextSwitch->initContext();
     }
 
     public function indexAction()
     {
-        // action body
+        if($this->_hasParam('Digits')){
+            $this->_setParam('order', $this->_getParam('Digits'));
+        }
+        
+        if($this->_hasParam('order')){
+            $order = Application_Model_Order::getOrder($this->_getParam('order'));
+            $this->view->order = $order;
+        }
     }
-
-
 }
 
